d235f0d6bdc354e8e56bf2276431fde7aa7dd0a6
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-10-19 08:16:53 +0530

    Added React Fragments

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,27 @@
 import food from "../images/burger-image.png";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const About = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div className="about-container">
-      <div className="about-left">
-        <h1>
-          Welcome to <br /> The world of <br /> <span>Tasty & Fresh Food</span>
-        </h1>
-        <h4>
-          "Better you will feel if you eat a Food<span>Fire</span> healthy meal"
-        </h4>
+    <>
+      <div className="about-container">
+        <div className="about-left">
+          <h1>
+            Welcome to <br /> The world of <br />{" "}
+            <span>Tasty & Fresh Food</span>
+          </h1>
+          <h4>
+            "Better you will feel if you eat a Food<span>Fire</span> healthy
+            meal"
+          </h4>
+        </div>
+        <div className="about-right">
+          <img src={food} alt="Food Image" />
+        </div>
       </div>
-      <div className="about-right">
-        <img src={food} alt="Food Image" />
-      </div>
-    </div>
+    </>
   );
 };
 
