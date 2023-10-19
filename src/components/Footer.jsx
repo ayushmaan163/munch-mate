99cb8c9598825019d07d5ae4cb136573226e49e9
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-10-19 07:18:07 +0530

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <>
-      <h4>Footer</h4>
-    </>
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a
+        href="https://github.com/ayushmaan163"
+        target="_blank"
+        title="Ayushmaan Jaiswal's GitHub Profile"
+      >
+        Ayushmaan Jaiswal
+      </a>
+      <i className="fa-solid fa-copyright"></i>2023
+      <strong>
+        Munch<span>Mate</span>
+      </strong>
+    </div>
   );
 };
 
