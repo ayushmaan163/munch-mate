8028831b17baeb6c0bea879872e3ecee225580c7
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-10-25 06:55:28 +0530

    Added Routes & Lazy Loading for the InstaMart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -8,6 +8,9 @@ import Error from "./components/Error";
 import Contact from "./components/Contact";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Instamart from "./components/Instamart";
+
+// const InstaMart = lazy(() => import("./components/Instamart"));
 
 const AppLayout = () => {
   return (
@@ -40,6 +43,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/restaurant/:resId",
         element: <RestaurantMenu />,
+      },
+      {
+        path: "/instamart",
+        element: (
+          <Suspense>
+            <Instamart />
+          </Suspense>
+        ),
       },
     ],
   },
