96bbd4dc3af0f37f47c4ac12dac21bd2e4df3d75
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-11-05 07:04:11 +0530

    Added the InstaMart component

--- src/components/Instamart.jsx
+++ src/components/Instamart.jsx
@@ -1,8 +1,52 @@
-const InstaMart = () => {
+import { useState } from "react";
+
+const Section = ({ title, description, isVisible, setIsVisible }) => {
+  // const [isVisible, setIsVisible] = useState(false);
   return (
     <>
-      <h3>MunchMate's InstaMart</h3>
-      <h4>Lazy Loading</h4>
+      <h2>{title}</h2>
+      {isVisible ? (
+        <div>
+          <button onClick={() => setIsVisible(null)}>Hide</button>
+          <p>{description}</p>
+        </div>
+      ) : (
+        <button onClick={() => setIsVisible(true)}>Show</button>
+      )}
+    </>
+  );
+};
+
+const InstaMart = () => {
+  const [visibleSection, setVisibleSection] = useState(null);
+  return (
+    <>
+      <Section
+        title={"About InstaMart"}
+        description={
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+        isVisible={visibleSection === "about"}
+        setIsVisible={() => setVisibleSection("about")}
+      />
+
+      <Section
+        title={"Team InstaMart"}
+        description={
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+        isVisible={visibleSection === "team"}
+        setIsVisible={() => setVisibleSection("team")}
+      />
+
+      <Section
+        title={"Career at InstaMart"}
+        description={
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+        }
+        isVisible={visibleSection === "career"}
+        setIsVisible={() => setVisibleSection("career")}
+      />
     </>
   );
 };
