5e2908980e49c4d262b8bb162768b645222da338
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-12-13 00:17:52 +0530

    Changed the wrong text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             <span>Tasty & Fresh Food</span>
           </h1>
           <h4>
-            "Better you will feel if you eat a Food<span>Fire</span> healthy
+            "Better you will feel if you eat a Munch<span>Mate</span> healthy
             meal"
           </h4>
         </div>
